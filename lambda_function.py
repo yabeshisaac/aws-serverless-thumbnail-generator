d77552dbcf66932c65570ebd7a26d74cd3322422
Yabesh <jyabeshyabesh@gmail.com>
2026-09-07 17:39:49 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,39 @@
+import io
+import os
+from urllib.parse import unquote_plus
+
+import boto3
+from PIL import Image
+
+s3 = boto3.client('s3')
+
+DEST_BUCKET = os.environ['DESTINATION_BUCKET']
+
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = unquote_plus(record['s3']['object']['key'])
+
+        response = s3.get_object(Bucket=bucket, Key=key)
+        image = Image.open(io.BytesIO(response['Body'].read()))
+
+        image.thumbnail((300, 300))
+
+        buffer = io.BytesIO()
+        image.save(buffer, "JPEG")
+        buffer.seek(0)
+
+        filename = key.split("/")[-1].split(".")[0]
+
+        s3.put_object(
+            Bucket=DEST_BUCKET,
+            Key=f"thumbnails/{filename}-thumbnail.jpg",
+            Body=buffer,
+            ContentType="image/jpeg"
+        )
+
+    return {
+        "statusCode": 200,
+        "body": "Thumbnail created successfully"
+    }
